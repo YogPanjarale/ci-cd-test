6022929b0f845f6fd4b039cae772ff7d82dcc16c
Yog <yog.panjarale@gmail.com>
2021-07-21 16:21:56 +0530

    more test

---
+++ src/testFileExists.js
@@ -0,0 +1,42 @@
+// console.log("Hello World")
+const user = process.env.GITHUB_ACTOR ||"YogPanjarale"
+const repo = process.env.GITHUB_REPOSITORY
+
+// load fs
+const fs = require("fs");
+const path = `./contributors/${user}`
+
+if (process.env.GITHUB_ACTIONS){
+
+    if (fs.existsSync(path)){
+        
+        // read the file
+        const content = fs.readFileSync(path);
+        const contributor = JSON.parse(content);
+        const keys = Object.keys(contributor);
+        if (contributor['github-username']!=user){
+            throw new Error(`${contributor['github-username']} does not match you username ${user}`)
+        }
+        if (keys!=[
+            'github-username',
+            'favourite-emoji',
+            'favourite-music',
+            'favourite-color'
+          ]){
+              throw new Error("All The Properties not present or extra properties given")
+          }
+    }else{
+        throw new Error(`File contributors/${user}.json does not exist`);
+    }
+}else{
+const obj={
+    "github-username": "YogPanjarale",
+    "favourite-emoji": "🍕",
+    "favourite-music": "https://open.spotify.com/track/6RM5WJcShIHsxrja11M1iK?si=e2d7fbad9eaf4bee",
+    "favourite-color": "#008081"
+}
+const map = Object.keys(obj);
+console.log(map)
+}
+
+// console.log(process.env.GITHUB_ACTOR)
