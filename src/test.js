b55c8552fd8cc3749157a93fec92909aa75b09cd
Yog <yog.panjarale@gmail.com>
2021-07-21 16:05:46 +0530

    test for file exisit or not

--- src/test.js
+++ src/test.js
@@ -1,2 +1,17 @@
 console.log("Hello World")
-console.log(process.env.GITHUB_ACTOR)
+const user = process.env.GITHUB_ACTOR
+const repo = process.env.GITHUB_REPOSITORY
+
+// load fs
+const fs = require("fs");
+const path = `./contributors/${user}`
+
+if (fs.existsSync(path)){
+
+    // read the file
+    const content = fs.readFileSync(path);
+}else{
+    throw new Error(`File contributors/${user}.json does not exist`);
+}
+
+// console.log(process.env.GITHUB_ACTOR)
